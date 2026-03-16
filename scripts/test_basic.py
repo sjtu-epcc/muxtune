cb60f819f593a0866ea7ae986859c3bf24091edd
DicardoX <1106828059@qq.com>
2026-03-16 23:15:54 +0800

    [hook] Pass hook test.

--- scripts/test_basic.py
+++ scripts/test_basic.py
@@ -18,13 +18,13 @@ class BasicFuncTest(unittest.TestCase):
     """
 
     # @unittest.skip("Pass.")
-    def test_lora_impl(self):
+    def test_correctness_of_spatial_temporal_task_colocation(self):
         import torch
 
         from muxtune.models.adapters.lora import LoraAdapter, LoraInputDispatcher, LoraOutputAggregator
         from muxtune.core.peft_modules import PeftModuleConfig, PeftModule, PeftModuleGroup
         from muxtune.core.utils import MixedTensor, BackwardThrottler
-        from muxtune.global_envs import PeftType, global_configs
+        from muxtune.global_envs import PeftType, global_configs, logger
 
         class DummyBackbone(torch.nn.Module):
             def __init__(self):
@@ -77,12 +77,12 @@ class BasicFuncTest(unittest.TestCase):
         os.environ["FORCED_ADAPTER_NAME_DEBUG"] = "peft_module_0::task_0"
         input_task_0 = MixedTensor({ 0: task_inputs[0] })
         peft_out_0 = backbone(input_task_0)
-        print(f"Single-task forward output of task_0: {peft_out_0}\n\n")
+        logger.info(f"Single-task forward output of task_0: {peft_out_0}\n\n")
 
         os.environ["FORCED_ADAPTER_NAME_DEBUG"] = "peft_module_0::task_1"
         input_task_1 = MixedTensor({ 0: task_inputs[1] })
         peft_out_1 = backbone(input_task_1)
-        print(f"Single-task forward output of task_1: {peft_out_1}\n\n")
+        logger.info(f"Single-task forward output of task_1: {peft_out_1}\n\n")
 
         del os.environ["FORCED_ADAPTER_NAME_DEBUG"]
         optimizer_0.zero_grad()
@@ -93,10 +93,10 @@ class BasicFuncTest(unittest.TestCase):
         loss_0.backward()
         loss_1.backward()
 
-        print(f"Single-task backward task_0 adapter grad: LoRA A: " + 
+        logger.info(f"Single-task backward task_0 adapter grad: LoRA A: " + 
               f"{peft_module.adapters['peft_module_0::task_0'].lora_A.weight.grad} " + 
               f"| LoRA B: {peft_module.adapters['peft_module_0::task_0'].lora_B.weight.grad}\n\n")
-        print(f"Single-task backward task_1 adapter grad: LoRA A: " + 
+        logger.info(f"Single-task backward task_1 adapter grad: LoRA A: " + 
               f"{peft_module.adapters['peft_module_0::task_1'].lora_A.weight.grad} " + 
               f" | LoRA B: {peft_module.adapters['peft_module_0::task_1'].lora_B.weight.grad}\n\n")
 
@@ -107,7 +107,7 @@ class BasicFuncTest(unittest.TestCase):
 
         batched_in = MixedTensor({ 0: torch.cat(task_inputs, dim=0) })
         batched_out = backbone(batched_in)
-        print(f"Multi-task batched forward output: {batched_out}")
+        logger.info(f"Multi-task batched forward output: {batched_out}")
 
         optimizer_0.zero_grad()
         optimizer_1.zero_grad()
@@ -119,10 +119,10 @@ class BasicFuncTest(unittest.TestCase):
             losses = MixedTensor({ 0: loss_0, 1: loss_1 })
             bw_throttler.backward(losses, peft_group_index)
 
-        print(f"Multi-task backward task_0 adapter grad: LoRA A: " + 
+        logger.info(f"Multi-task backward task_0 adapter grad: LoRA A: " + 
               f"{peft_module.adapters['peft_module_0::task_0'].lora_A.weight.grad} " + 
               f"| LoRA B: {peft_module.adapters['peft_module_0::task_0'].lora_B.weight.grad}\n\n")
-        print(f"Multi-task backward task_1 adapter grad: LoRA A: " + 
+        logger.info(f"Multi-task backward task_1 adapter grad: LoRA A: " + 
               f"{peft_module.adapters['peft_module_0::task_1'].lora_A.weight.grad} " + 
               f" | LoRA B: {peft_module.adapters['peft_module_0::task_1'].lora_B.weight.grad}\n\n")
         
@@ -131,6 +131,7 @@ class BasicFuncTest(unittest.TestCase):
         self.assertTrue(torch.allclose(single_a1, peft_module.adapters["peft_module_0::task_1"].lora_A.weight.grad, rtol=1e-2, atol=1e-2), "task_1 lora_A: single vs batched")
         self.assertTrue(torch.allclose(single_b1, peft_module.adapters["peft_module_0::task_1"].lora_B.weight.grad, rtol=1e-2, atol=1e-2), "task_1 lora_B: single vs batched")
 
+        logger.info("Passed correctness test for spatial-temporal multi-task cololocation.")
 
 if __name__ == '__main__':
     unittest.main()
